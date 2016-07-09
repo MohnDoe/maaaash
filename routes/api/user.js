b0a7023d2e2a7ffbb3aad2e7db79906bc2bb4599
Kevin F. P <kfnpfeffer@gmail.com>
2016-07-10 00:28:16 +0200

    defining routes & creating pointsOperators

---
+++ routes/api/user.js
@@ -0,0 +1,35 @@
+var express = require('express'),
+	router = express.Router();
+
+var usersController = require('../../controllers/usersController');
+
+/* USERS GET */
+// get info about connected user
+router.get('/');
+// get a user by ID
+router.get('/:id');
+// get count of all user
+router.get('/all');
+// get count of all deleted user
+router.get('/deleted');
+/* END USERS GET */
+
+/* USERS POST */
+// create a custom user
+router.post('/');
+
+/* END USERS POST */
+
+
+/* USERS PUT */
+
+/* END USERS PUT*/
+
+/* USERS DELETE */
+// delete one user
+router.delete('/:id');
+/* END USERS DELETE */
+
+
+
+module.exports = router;
--- routes/api/vote.js
+++ routes/api/vote.js
@@ -5,36 +5,37 @@ var votesController = require('../../controllers/votesController');
 // var usersController = require('../../controllers/usersController');
 
 /* VOTES GET */
-// get list of all votes
+// get a vote for a logged user
 router.get('', votesController.getVote);
-// get a vote
-router.get('/:id');
-// get COUNT of likes & dislikes on a vote
-router.get('/:id/likes');
+// get a vote by the hash_id
+router.get('/:hash_id');
+// get all votes corresponding to this identifier
+router.get('/i/:identifier');
+// get count of all completed vote
+router.get('/completed');
+// get count of all vote
+router.get('/all');
+// get count of all deleted vote
+router.get('/deleted');
 /* END VOTES GET */
 
 /* VOTES POST */
-// add a vote
-router.post('');
-// add a song to a vote
-router.post('/:id/song');
-// add an artists from a vote
-router.post('/:id/artist');
+// create a custom vote
+router.post('/'); // might not do that
+
 /* END VOTES POST */
 
 
 /* VOTES PUT */
-// edit a vote
-router.put('/:id');
-// increment popularity of a vote
-router.put('/:id/popularity');
-// increment views count of a vote
-router.put('/:id/views');
+// set winner to a vote
+router.put('/:hash_id/:winner');
+// set a vote to a draw
+router.put('/:hash_id/draw');
 /* END VOTES PUT*/
 
 /* VOTES DELETE */
 // delete one vote
-router.delete('/:id');
+router.delete('/:hash_id');
 /* END VOTES DELETE */
 
 
