0609162eefe3aa53f63a3032cb22555b736ec6a0
Kevin F. P <kfnpfeffer@gmail.com>
2016-07-20 23:25:31 +0200

    hopfully fixed UniqueError with sequelize

--- src/server/operators/channelsOperators.js
+++ src/server/operators/channelsOperators.js
@@ -32,21 +32,36 @@ function createChannel(data) {
 
 function findOrCreateChannel(data) {
 	return new Promise(function(resolve, reject) {
-		return Models.channel.find({
-			where: {
-				channel_id: data.id
-			}
-		}).then(function(channel) {
-			if (channel) {
+		return Models.channel.findOrCreate({
+				where: {
+					channel_id: data.id
+				},
+				defaults: {
+					channel_id: data.id,
+					name: data.name,
+					channel_id: data.id,
+					thumbnail_url: data.thumbnail_url,
+					description: data.description,
+					view_count: data.view_count,
+					subscriber_count: data.subscriber_count,
+					video_count: data.video_count,
+					thumbnail_url: data.thumbnail_url,
+					custom_url: data.custom_url,
+					lang: data.lang,
+					country: data.country,
+					hidden_subscriber_count: data.hidden_subscriber_count,
+					published_at: data.published_at
+				}
+			})
+			.spread(function(channel, created) {
 				resolve(channel);
-			} else {
-				return createChannel(data);
-			}
-		}).then(function(channel) {
-			resolve(channel);
-		}).catch(function(err) {
-			reject(err);
-		})
+			})
+			.catch(function(err) {
+				console.log("An error during creation of channel! FOC");
+				console.log(err.message);
+
+				reject(err);
+			})
 	})
 }
 
